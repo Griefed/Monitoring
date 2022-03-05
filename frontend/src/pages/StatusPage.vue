cc057f0cf99e7c40f0a8dc8f16ab2db1983f8c30
Griefed <griefed@griefed.de>
2022-03-05 18:18:06 +0100

    improv(Status Page): Improve visuals and identification of problematic hosts.

--- frontend/src/pages/StatusPage.vue
+++ frontend/src/pages/StatusPage.vue
@@ -4,45 +4,61 @@
 
     <!-- HOSTS WHICH ARE UP -->
     <div class="row flex-center transparent" style="padding-bottom: 120px;">
-      <q-card flat style="margin: 5px; width: 100%; max-width: 400px;" v-for="host in hostsDown" v-bind:key="host">
+      <q-card flat :style="'margin: 5px; width: 100%; max-width: 400px; height: 120px; background: ' + getCssColor(host.status, host.hostAvailable) + ';'" v-for="host in hostsDown" v-bind:key="host">
         <q-list>
 
           <q-item-label
             :class="this.$q.platform.is.mobile ? 'text-weight-bolder text-h6 text-center' : 'text-weight-bolder text-h6 text-center'"
             :style="this.$q.platform.is.mobile ? 'font-size: 18px;' : 'padding-top: 5px; padding-bottom: 5px;'"
           >
-            {{host.name}}
             <q-badge
               v-if="host.code != 418"
-              class="q-mr-xs right"
-              text-color="white"
-              rounded
-              align="bottom"
-              color="red"
+              class="q-mr-xs"
+              style="font-size: 16px;"
               floating
-            >
-              {{ host.code }}
-            </q-badge>
+              :label="host.code"
+            />
+            {{ host.name }}<br>
+            <q-chip
+              outline
+              dense
+              v-ripple
+              clickable
+              :label="host.address"
+              @click="openURL(host.address)"
+            />
           </q-item-label>
 
-          <q-separator/>
+          <q-separator
+            inset
+            size="4px"
+          />
 
           <q-item>
             <q-item-section top>
-              <q-item-label lines="3">
-                <span class="text-weight-bolder text-red-8 right">{{ host.status }}</span>
-                <span class="text-weight-bolder">{{host.address}}</span><br>
+              <q-item-label lines="2">
+                <span class="text-weight-bolder right" style="padding-top: 11px; font-size: 17px;">
+                  {{ host.status }}
+                </span>
                 <q-chip
                   v-if="host.hostAvailable && host.ip !== 'null'"
                   class="text-weight-bolder"
+                  size="lg"
                   dense
+                  square
+                  flat
+                  unelevated
                   color="green"
                   :label="host.ip"
                 />
                 <q-chip
                   v-if="!host.hostAvailable && host.ip !== 'null'"
                   class="text-weight-bolder"
+                  size="lg"
                   dense
+                  square
+                  flat
+                  unelevated
                   color="red"
                   :label="host.ip"
                 />
@@ -59,16 +75,30 @@
       <q-scroll-area class="vertical-bottom window-width" style="height: 120px; width: 100%">
         <q-card-section class="row wrap flex-center">
 
-          <q-chip outline v-ripple v-for="host in hostsOk" v-bind:key="host" style="margin: 10px; width: 100%; max-width: 200px;">
+          <q-chip
+            outline
+            v-ripple
+            v-for="host in hostsOk" v-bind:key="host"
+            style="margin: 10px; width: 100%; max-width: 200px;"
+            clickable
+            @click="openURL(host.address)"
+          >
             <q-item-section>
               <q-item-label class="text-center text-weight-bolder">
-                <q-badge rounded color="green" floating>{{ host.status }}</q-badge>
+                <q-badge
+                  rounded
+                  :color="getQuasarColor(host.status, host.hostAvailable)"
+                  floating
+                >
+                  {{ host.status }}
+                </q-badge>
                 {{ host.name }}
               </q-item-label>
             </q-item-section>
             <q-tooltip v-if="!this.$q.platform.is.mobile">
               {{ host.address }}
             </q-tooltip>
+
           </q-chip>
 
         </q-card-section>
@@ -80,8 +110,8 @@
 
 <script>
 import { hosts,api } from "boot/axios";
-import { defineComponent, inject } from 'vue';
-import { ref } from 'vue';
+import { defineComponent, inject, ref } from 'vue';
+import { getCssVar,openURL } from 'quasar';
 
 export default defineComponent({
   name: 'StatusPage',
@@ -92,14 +122,36 @@ export default defineComponent({
     let hostsOk = ref(null);
     let hostsDown = ref(null);
 
+    let ipRegEx = new RegExp('\\d+\\.\\d+\\.\\d+\\.\\d+')
+
     return {
       store,
       hostsOk,
       hostsDown,
-      polling: 5000
+      polling: 5000,
+      ipRegEx
     }
   },
   methods: {
+    openURL(address) {
+
+      if (this.ipRegEx.test(address)) {
+        address = 'http://' + address;
+      }
+
+      openURL(
+        address,
+        null,
+        {
+          noopener: true, // this is set by default for security purposes
+                          // but it can be disabled if specified with a Boolean false value
+          menubar: false,
+          toolbar: false,
+          noreferrer: false,
+          // .....any other window features
+        }
+      )
+    },
     updateHosts() {
       hosts.get().then(response => {
 
@@ -108,7 +160,7 @@ export default defineComponent({
         let down = [];
 
         hosts.forEach(host => {
-          if (host.code === 200) {
+          if (host.code === 200 || host.code === 301) {
             ok.push(host);
           } else {
             down.push(host);
@@ -125,6 +177,102 @@ export default defineComponent({
         console.log("Encountered an error fetching host information: " + error);
 
       });
+    },
+    getCssColor(status, available) {
+
+      if (!available && status !== "OK") {
+        // red-10 #ad1e1f
+        return '#ad1e1f';
+      }
+
+      switch (status) {
+// CRITICAL
+        case "UNAVAILABLE":
+
+          // blue-grey #647d8b
+          return "#647d8b";
+
+        case "SSL HANDSHAKE ERROR":
+
+          // deep-orange-13 #f13e0e
+          return '#f13e0e';
+
+        case "NOT FOUND":
+
+          // orange-6 #f59711
+          return '#f59711';
+
+        case "CONNECTION REFUSED":
+
+          // lime-6 #ceda3d
+          return '#ceda3d';
+
+// OK / ACCEPTABLE / CONFIG ERROR
+        case "OK":
+
+          return getCssVar('positive');
+
+        case "REDIRECT":
+
+          return getCssVar('info');
+
+        case "INVALID URL":
+
+          // grey-13 #bdbdbd
+          return '#bdbdbd';
+
+// EVERYTHING ELSE
+        default:
+          return getCssVar('primary');
+      }
+    },
+    getQuasarColor(status, available) {
+
+      if (!available && status !== "OK") {
+        // red-10 #ad1e1f
+        return 'red-10';
+      }
+
+      switch (status) {
+// CRITICAL
+        case "UNAVAILABLE":
+
+          // blue-grey #647d8b
+          return "blue-grey";
+
+        case "SSL HANDSHAKE ERROR":
+
+          // deep-orange-13 #f13e0e
+          return 'deep-orange-13';
+
+        case "NOT FOUND":
+
+          // orange-6 #f59711
+          return 'orange-6';
+
+        case "CONNECTION REFUSED":
+
+          // lime-6 #ceda3d
+          return 'lime-6';
+
+// OK / ACCEPTABLE / CONFIG ERROR
+        case "OK":
+
+          return "positive";
+
+        case "REDIRECT":
+
+          return "info";
+
+        case "INVALID URL":
+
+          // grey-13 #bdbdbd
+          return 'grey-13';
+
+// EVERYTHING ELSE
+        default:
+          return "primary";
+      }
     }
   },
   mounted() {
